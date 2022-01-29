151b198b5c9ab4937484c124d39c05014d7b898d
guest0417 <48578453+guest0417@users.noreply.github.com>
2022-01-29 18:53:48 +0800

    Add files via upload

---
+++ 健康上報.py
@@ -0,0 +1,127 @@
+import requests
+import sys
+import os
+import io
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/..")
+
+import datetime
+import logging
+import json
+from html.parser import HTMLParser
+from email.mime.text import MIMEText
+from email.header import Header
+import smtplib
+
+logger = logging.getLogger()
+log_str = io.StringIO()
+fh = logging.StreamHandler(log_str)
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+# Third-party SMTP service for sending alert emails. 第三方 SMTP 服务，用于发送告警邮件
+mail_host = "smtp.qq.com"       # SMTP server, such as QQ mailbox, need to open SMTP service in the account. SMTP服务器,如QQ邮箱，需要在账户里开启SMTP服务
+mail_user = "2590187490@qq.com"  # Username 用户名
+mail_pass = "fprrvocwgjojebbc"  # Password, SMTP service password. 口令，SMTP服务密码
+mail_port = 465  # SMTP service port. SMTP服务端口
+
+class token_extract(HTMLParser):
+    data = None
+    #继承HTMLParser类
+    def __init__(self):
+        #用父类的__init__
+        HTMLParser.__init__(self)
+    def handle_starttag(self, tag, attrs):
+        # 重写handle_starttag方法
+        if tag=="input":
+           for key,values in attrs:
+               if (key=="value" and len(values)==108):
+                   self.data = values
+                   print("extracted token:", values)
+    def get_token(self):
+        return self.data
+
+def sendEmail(fromAddr, toAddr, subject, content):
+    sender = fromAddr
+    receivers = toAddr
+    message = MIMEText(content, 'plain', 'utf-8')
+    message['From'] = Header(fromAddr, 'utf-8')
+    message['To'] = Header(toAddr, 'utf-8')
+    message['Subject'] = Header(subject, 'utf-8')
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_host, mail_port)
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print("execution completed")
+        return True
+    except smtplib.SMTPException as e:
+        print(e)
+        print("Error: send email fail")
+        return False
+
+def main_handler(event, context):
+    resp = None
+    url="https://oa.shsmu.edu.cn/WProtalInfo_WeChat/InformationReporting/InformationReporting"
+    logger.info('Accessing：'+url)
+    try:
+        resp = requests.get(url, timeout=3)
+        logger.info(resp)
+    except(requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout) as e:
+        logger.warning("request exceptions:" + str(e))
+    else:
+        if resp.status_code >= 400:
+            logger.warn("response status code fail:" + str(resp.status_code))
+    token_extract_obj = token_extract()
+    token_extract_obj.feed(resp.text)
+    token = token_extract_obj.get_token()
+    logger.info("token acquired："+ token)
+    data = {'__RequestVerificationToken' : token,
+            'studentNo':'520712910019', 'studentName':'陳鎧之',
+            'mobile':'null;userAgent:WeChat;os:null;Webkit:537.36',
+            'Loction':'上海市黄浦区淮海中路街道合肥路365号上海交通大学医学院东区',
+            'LoctionSatus':'true',
+            'Country':'中国',
+            'Province':'上海市',
+            'City':'上海市',
+            'AtSchool':'true',
+            'rSchool':'true',
+            'Accommodation':'1',
+            'Fever':'false',
+            'rfever':'false',
+            'Temperature':'℃',
+            'Quarantine':'false',
+            'rQuarantine':'false',
+            'QAdress':'',
+            'SuspectedOrConfirmed':'0',
+            'rsoc':'0',
+            'hesuanState':'02',
+            'hesuan':'02',
+            'checkHSReason':'',
+            'HSReasonDesc':'',
+            'Remark':'',
+            'X-Requested-With':'XMLHttpRequest'
+            }
+    logger.info("forwarding data: https://oa.shsmu.edu.cn/WProtalInfo_WeChat/InformationReporting/InformationReporting")
+    try :
+        post = requests.post(url, data = data, timeout = 3)
+        logger.info(post)
+    except(requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout) as e:
+        logger.warn("request exceptions:" + str(e))
+    else:
+        if post.status_code >= 400:
+            logger.warning("response status code fail:" + str(resp.status_code))
+    logger.info("post completed：" + post.text)
+    subject = "Please note: Today's health check"
+    logger.info("done, sending message" + post.text + "to thomaschan32866@gmail.com")
+    log_contents = log_str.getvalue()
+    sendEmail(mail_user, "thomaschan32866@gmail.com", subject, log_contents.lower())
+    log_str.close()
+    return True
+
+if __name__ == "__main__":
+    main_handler(None,None)
